cad0de5c6f2555c88a95ab18d5f667b9a2bdbbcf
Subham Verma <shubhamverma17092005@gmail.com>
2026-06-19 19:47:07 +0530

    feat: add bank account object with deposit, withdraw and getBalance methods

---
+++ index.js
@@ -0,0 +1,23 @@
+const account = {
+    owner: "Blur",
+    balance: 0,
+    deposit: function(num){
+        if (num <= 0) return "Invalid amount";
+        else this.balance += num;
+    },
+    withdraw: function(num){
+        if (num <= 0) return "Invalid amount";
+        else if (num > this.balance) return "Insufficient funds";
+        else this.balance -= num;
+    },
+    getBalance: function(){
+        return `Owner: ${this.owner} | Balance: $${this.balance}`;
+    }
+}
+
+account.deposit(1000);
+console.log(account.deposit(-50));
+account.withdraw(200);
+console.log(account.withdraw(900));
+console.log(account.withdraw(0));
+console.log(account.getBalance());
